40f03bc679a6a4ed062ff1e2c08f3f26574288dd
Zachary Sunberg <sunbergzach@gmail.com>
2017-05-23 00:36:54 -0700

    added pomdps stuff

---
+++ pomdps/get_requirements.jl
@@ -0,0 +1,29 @@
+# will install the requirements for the notebooks
+
+function add_if_missing(pkg, installed)
+    if !in(pkg, installed)
+        Pkg.add(pkg)
+    end
+end
+
+packages = keys(Pkg.installed())
+
+add_if_missing("POMDPs", packages)
+add_if_missing("POMDPToolbox", packages)
+add_if_missing("POMDPModels", packages)
+add_if_missing("ParticleFilters", packages)
+add_if_missing("Distributions", packages)
+add_if_missing("Interact", packages)
+
+using POMDPs
+
+if !in("QMDP", packages)
+    POMDPs.add("QMDP")
+end
+if !in("POMCPOW", packages)
+    POMDPs.add("POMCPOW")
+end
+if !in("LaserTag", packages)
+    Pkg.clone("https://github.com/zsunberg/LaserTag.jl.git")
+    Pkg.build("LaserTag")
+end
